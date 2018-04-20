c632dd7d10d5e3334b1d64f48d9a209d360c092f
joju <whwndud92@gmail.com>
2018-04-20 18:40:48 +0900

    mysql_connection test

--- Server/cmake_install.cmake
+++ Server/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/joju/CLionProjects/multichatting/Server
+# Install script for directory: /Users/joju/CLionProjects/multichatting/server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/joju/CLionProjects/multichatting/Server/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/joju/CLionProjects/multichatting/server/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
